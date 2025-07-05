00ffc091ea37c99d9f133986c16209ee300cee30
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-07-05 13:46:20 -0400

    changing login/signup for authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import Feed from "@/components/Feed";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  if (!session) redirect("/sign-in");
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4 text-center">
-        Welcome to Sweat Check Connect
-      </h1>
-      <p className="text-lg mb-8">
-        Your hub for fitness challenges, shared playlists, and more!
-      </p>
-      <Image
-        src="/images/workoutpost.jpeg"
-        alt="Fitness Logo"
-        width={200}
-        height={200}
-        className="rounded-full"
-      />
+    // <div className="flex flex-col items-center justify-center min-h-screen">
+    //   <h1 className="text-4xl font-bold mb-4 text-center">
+    //     Welcome to Sweat Check Connect
+    //   </h1>
+    //   <p className="text-lg mb-8">
+    //     Your hub for fitness challenges, shared playlists, and more!
+    //   </p>
+    //   <Image
+    //     src="/images/workoutpost.jpeg"
+    //     alt="Fitness Logo"
+    //     width={200}
+    //     height={200}
+    //     className="rounded-full"
+    //   />
+    // </div>
+
+    <div className="flex h-screen">
+      <Feed />
     </div>
   );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,3 @@
+import NextAuth from "next-auth";
+
+export const { auth, handlers } = NextAuth({ providers: [] });
