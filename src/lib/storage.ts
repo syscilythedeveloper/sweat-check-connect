5c96f423571cdc0e8014abbb37dae212309f98b9
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-07-08 16:30:29 -0400

    adding videos to blob storage

--- src/lib/storage.ts
+++
@@ -1,6 +0,0 @@
-//write function to upload video to get url from aws s3 or vercel blob
-
-export function uploadVideoToStorage(file: File): Promise<string> {
-  const url = `https://your-storage-service.com/upload/${file.name}`;
-  return Promise.resolve(url);
-}
