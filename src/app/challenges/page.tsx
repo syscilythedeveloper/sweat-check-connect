9109c48faedfe9de9dfdd737ef94a6e20f5de7ee
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-07-30 15:11:33 -0400

    larger card gap for mobile

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -246,7 +246,7 @@ const ChallengesPage = () => {
       )}
 
       {/* Challenges Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 ">
         {filteredChallenges.map((challenge) => (
           <ChallengeCard
             key={challenge.id}
