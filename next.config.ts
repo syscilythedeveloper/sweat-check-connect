5d428ce6de8090d0cae90b21d564367fa344f943
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-08-05 06:00:40 -0400

    allowign blob images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["img.clerk.com", "images.clerk.dev"],
+    domains: [
+      "img.clerk.com",
+      "images.clerk.dev",
+      "qqahaz9rntmohbfm.public.blob.vercel-storage.com",
+    ],
   },
 };
 
